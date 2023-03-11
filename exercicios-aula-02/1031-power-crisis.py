1d2f26cd5ee00c108eef0bd8e4ee6302b0436629
Wellington <wgawebmail@gmail.com>
2023-03-10 23:00:38 -0300

    feat: aula 02 - 1031 - adiciona estrutura de repetição

--- exercicios-aula-02/1031-power-crisis.py
+++ exercicios-aula-02/1031-power-crisis.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
 n = int(input())
-cities = list(range(1, n+1))
-first_city = 1
-last_city = 13
 
-for m in cities:
-    random_list = []
-    current_city = first_city
-    for city in cities:
-        if current_city > n:
-            current_city = current_city - n
-        random_list.append(current_city)
-        current_city = current_city + m
-    if random_list[-1] == last_city:
-        print(random_list)
-        print(m)
-        break
+while n > 0:
+    cities = list(range(1, n+1))
+    first_city = 1
+    last_city = 13
+
+    for m in cities:
+        random_list = []
+        current_city = first_city
+        for city in cities:
+            if current_city > n:
+                current_city = current_city - n
+            random_list.append(current_city)
+            current_city = current_city + m
+        if random_list[-1] == last_city:
+            print(random_list)
+            print(m)
+            break
+
+    n = int(input())
